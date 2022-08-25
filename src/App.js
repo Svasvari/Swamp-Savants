12a6923d08441a9a94427f5e511241619dd0b8b5
Sean Vasvari <vasvarisean@gmail.com>
2022-08-24 20:42:41 -0400

    started work on page slider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Parallax from "parallax-js";
 import React, { useRef, useEffect, useState } from "react";
-import $ from 'jquery';
+import $ from 'jquery'
 
 import Landing from './Pages/Landing';
 import LoudoutRandomizer from './Pages/loudoutRandomizer';
@@ -10,57 +10,61 @@ import PrestigeRoulette from './Pages/prestigeRoulette';
 
 function App() {
 
-  const [page, setPage] = useState(1);
-
+  const [page, setPage] = useState(0);
 
   return (
-    <Router>
+    <div>
       <nav className="navigationWrapper">
-        <a className='link-logo' href='/'><div className="logoWrapper">
+        <div className="logoWrapper" onClick={() => setPage(0)}>
           <span className="stylish">Swamp</span>
           <span className="logo">Savant</span>
-        </div></a>
+        </div>
         <ul className="navigation">
-          <li className="parent" onClick={() => setPage(2)}>Loadout Randomizer</li>
-          <li className="parent" onClick={() => setPage(3)}>Prestige Roulette</li>
+          <li className="parent" onClick={() => setPage(1)}>Loadout Randomizer</li>
+          <li className="parent" onClick={() => setPage(2)}>Prestige Roulette</li>
           <li className="parent"><a className="link" href="/about">About</a></li>
           <li className="parent"><a className="link" href="/contact">Contact</a></li>
         </ul>
       </nav>
-      {/* {page === 1 ? (
+      {page === 0 ? (
         <Landing />)
-        : 
+        :
+        (<></>)
+      }
+      {page === 1 ? (
+        <LoudoutRandomizer />)
+        :
         (<></>)
       }
       {page === 2 ? (
-        <LoudoutRandomizer />)
-        : 
+        <PrestigeRoulette />)
+        :
         (<></>)
-      } */}
+      }
 
       <div class="slider">
-        <input type="radio" name="slider" title="slide1" checked="checked" class="slider__nav" />
-        <input type="radio" name="slider" title="slide2" class="slider__nav" />
-        <input type="radio" name="slider" title="slide3" class="slider__nav" />
-        <input type="radio" name="slider" title="slide4" class="slider__nav" />
-        <div class="slider__inner">
-          <div class="slider__contents"><i class="slider__image fa fa-codepen"></i>
-            <Landing />
+        <div class="items-group">
+          <div class="item">
           </div>
-          <div class="slider__contents"><i class="slider__image fa fa-newspaper-o"></i>
-            <LoudoutRandomizer />
+          <div class="item">
           </div>
-          <div class="slider__contents"><i class="slider__image fa fa-television"></i>
-            <PrestigeRoulette />
-          </div>
-          <div class="slider__contents"><i class="slider__image fa fa-television"></i>
-            <PrestigeRoulette />
+          <div class="item">
           </div>
         </div>
+
+        <div class="navigations">
+          <ul class="dots">
+            <li id='dot1' onClick={() => {
+              let elem = document.getElementById('dot1');
+              elem.className = '.slider .navigations .dots li.active'
+              setPage(0);
+              }}/>
+            <li className='dot2' onClick={() => setPage(1)}/>
+            <li className='dot3' onClick={() => setPage(2)}/>
+          </ul>
+        </div>
       </div>
-      <Routes>
-      </Routes>
-    </Router >
+    </div>
   );
 }
 
