02b05a5b00d2859762c7969ce206e382a1994118
Sean Vasvari <vasvarisean@gmail.com>
2022-08-29 09:05:58 -0400

    Finished basic loudout randomizer logic and a few filter options

--- src/Pages/loudoutRandomizer.js
+++ src/Pages/loudoutRandomizer.js
@@ -1,3 +1,4 @@
+import { toBePartiallyChecked } from "@testing-library/jest-dom/dist/matchers";
 import React, { useRef, useEffect, useState } from "react";
 
 const LoudoutRandomizer = () => {
@@ -25,18 +26,82 @@ const LoudoutRandomizer = () => {
 
     ];
 
-    //STATES
+    const tools = [
+        { name: 'Alert Trip Mine', image: '' },
+        { name: 'Blank Fire Decoys', image: '' },
+        { name: 'Choke Bomb', image: '' },
+        { name: 'Concertina Trip Mine', image: '' },
+        { name: 'Decoy Fuses', image: '' },
+        { name: 'Decoys', image: '' },
+        { name: 'Dusters', type: 'melee', image: '' },
+        { name: 'Electric Lamp', image: '' },
+        { name: 'First Aid Kit', image: '' },
+        { name: 'Flare Pistol', image: '' },
+        { name: 'Fusees', image: '' },
+        { name: 'Heavy Knife', type: 'melee', image: '' },
+        { name: 'Knife', type: 'melee', image: '' },
+        { name: 'Knuckle Knife', type: 'melee', image: '' },
+        { name: 'Poison Trip Mine', image: '' },
+        { name: 'Quad Derringer', image: '' },
+        { name: 'Spyglass', image: '' },
+        { name: 'Throwing Knives', image: '' },
+    ];
+
+    const consumables = [
+        { name: 'Ammo Box', image: '' },
+        { name: 'Antidote Shot', image: '' },
+        { name: 'Big Dynamite Bundle', image: '' },
+        { name: 'Chaos Bomb', image: '' },
+        { name: 'Concertina Bomb', image: '' },
+        { name: 'Dynamite Bundle', image: '' },
+        { name: 'Dynamite Stick', image: '' },
+        { name: 'Fire Bomb', image: '' },
+        { name: 'Flash Bomb', image: '' },
+        { name: 'Frag Bomb', image: '' },
+        { name: 'Hellfire Bomb', image: '' },
+        { name: 'Hive Bomb', image: '' },
+        { name: 'Liquid Fire Bomb', image: '' },
+        { name: 'Poison Bomb', image: '' },
+        { name: 'Stamina Shot', image: '' },
+        { name: 'Sticky Bomb', image: '' },
+        { name: 'Vitality Shot', image: '' },
+        { name: 'Waxed Dynamite Stick', image: '' },
+        { name: 'Weak Antidote Shot', image: '' },
+        { name: 'Weak Stamina Shot', image: '' },
+        { name: 'Weak Vitality Shot', image: '' },
+    ];
+
+    //STATES///////////////////////////////////////////////////////////////////////////////
+
+    //Main States
     const weaponsMasterList = [].concat(weaponsSmall, weaponsMedium, weaponsLarge);
     const [weaponsPool, setWeaponsPool] = useState([].concat(weaponsSmall, weaponsMedium, weaponsLarge));
+    const [toolPool, setToolPool] = useState(tools);
 
+    //Toggle Options
     const [quartermaster, setQuartermaster] = useState(false);
-    const [customAmmo, setCustomAmmo] = useState(true);
+    const [customAmmo, setCustomAmmo] = useState(false);
     const [disableScopes, setDisableScopes] = useState(false);
+    const [fixedMedkit, setFixedMedkit] = useState(false);
+    const [fixedMeleeTool, setFixedMeleeTool] = useState(false);
 
+    //Weapons
     const [weaponOne, setWeaponOne] = useState("N/A");
     const [weaponOneAmmo, setWeaponOneAmmo] = useState('N/A');
     const [weaponTwo, setWeaponTwo] = useState("N/A");
     const [weaponTwoAmmo, setWeaponTwoAmmo] = useState('N/A');
+
+    //Tools
+    const [toolOne, setToolOne] = useState('N/A');
+    const [toolTwo, setToolTwo] = useState('N/A');
+    const [toolThree, setToolThree] = useState('N/A');
+    const [toolFour, setToolFour] = useState('N/A');
+
+    //Consumables
+    const [consumableOne, setConsumableOne] = useState('N/A');
+    const [consumableTwo, setConsumableTwo] = useState('N/A');
+    const [consumableThree, setConsumableThree] = useState('N/A');
+    const [consumableFour, setConsumableFour] = useState('N/A');
 
     //FUNCTIONS/////////////////////////////////////////////////////////////////////////////
 
@@ -52,7 +117,8 @@ const LoudoutRandomizer = () => {
     }
 
     //Generate Loudout
-    const roll = () => {  
+    const roll = () => {
+        //Roll Weapons
         let randomWeaponOne = '';
         let randomWeaponOneAmmo = '';
         let randomWeaponTwo = '';
@@ -70,31 +136,86 @@ const LoudoutRandomizer = () => {
             randomWeaponTwoAmmo = customAmmo ? randomWeaponTwo.specialAmmo[Math.floor(Math.random() * randomWeaponTwo.specialAmmo.length)] : '';
             setWeaponTwo(randomWeaponTwo);
             setWeaponTwoAmmo(randomWeaponTwoAmmo);
+        } else {
+            //Roll Weapon One
+            randomWeaponOne = weaponsPool[Math.floor(Math.random() * weaponsPool.length)];
+            randomWeaponOneAmmo = customAmmo ? randomWeaponOne.specialAmmo[Math.floor(Math.random() * randomWeaponOne.specialAmmo.length)] : '';
+            setWeaponOne(randomWeaponOne);
+            setWeaponOneAmmo(randomWeaponOneAmmo);
 
-            return;
+            //Roll Weapon Two
+            let weapon2Pool = [];
+            if (randomWeaponOne.size === 'L') {
+                weapon2Pool = weaponsPool.filter((weapon) => weapon.size === 'S');
+            } else if (randomWeaponOne.size === 'M') {
+                weapon2Pool = weaponsPool.filter((weapon) => weapon.size !== 'L');
+            } else if (randomWeaponOne.size === 'S') {
+                weapon2Pool = weaponsPool;
+            }
+            randomWeaponTwo = weapon2Pool[Math.floor(Math.random() * weapon2Pool.length)];
+            randomWeaponTwoAmmo = customAmmo ? randomWeaponTwo.specialAmmo[Math.floor(Math.random() * randomWeaponTwo.specialAmmo.length)] : '';
+            setWeaponTwo(randomWeaponTwo);
+            setWeaponTwoAmmo(randomWeaponTwoAmmo);
         }
 
-        //Roll Weapon One
-        randomWeaponOne = weaponsPool[Math.floor(Math.random() * weaponsPool.length)];
-        randomWeaponOneAmmo = customAmmo ? randomWeaponOne.specialAmmo[Math.floor(Math.random() * randomWeaponOne.specialAmmo.length)] : '';
-        setWeaponOne(randomWeaponOne);
-        setWeaponOneAmmo(randomWeaponOneAmmo);
+        //Roll Tools
+        let randomToolOne = '';
+        let randomToolTwo = '';
+        let randomToolThree = '';
+        let randomToolFour = '';
+        let currentToolPool = toolPool;
 
-        //Roll Weapon Two
-        let weapon2Pool = [];
-        if (randomWeaponOne.size === 'L') {
-            weapon2Pool = weaponsPool.filter((weapon) => weapon.size === 'S');
-        } else if (randomWeaponOne.size === 'M') {
-            weapon2Pool = weaponsPool.filter((weapon) => weapon.size !== 'L');
-        } else if (randomWeaponOne.size === 'S') {
-            weapon2Pool = weaponsPool;
+        //Tool 1
+        if(fixedMedkit) {
+            randomToolOne = tools.filter((tool) => tool.name === 'First Aid Kit')
+            setToolOne(randomToolOne[0]);
+            currentToolPool = currentToolPool.filter((tool) => tool.name !== 'First Aid Kit');
+        } else {
+            randomToolOne = currentToolPool[Math.floor(Math.random() * currentToolPool.length)];
+            setToolOne(randomToolOne);
+            currentToolPool = currentToolPool.filter((tool) => tool.name !== randomToolOne.name);
         }
-        randomWeaponTwo = weapon2Pool[Math.floor(Math.random() * weapon2Pool.length)];
-        randomWeaponTwoAmmo = customAmmo ? randomWeaponTwo.specialAmmo[Math.floor(Math.random() * randomWeaponTwo.specialAmmo.length)] : '';
-        setWeaponTwo(randomWeaponTwo);
-        setWeaponTwoAmmo(randomWeaponTwoAmmo);
+        //Tool 2
+        if(fixedMeleeTool) {
+            let randomMeleeTools = toolPool.filter((tool) => tool.type === 'melee')
+            randomToolTwo = randomMeleeTools[Math.floor(Math.random() * randomMeleeTools.length)];
+            setToolTwo(randomToolTwo);
+            currentToolPool = currentToolPool.filter((tool) => tool.name !== randomToolTwo.name);
+        } else {
+            randomToolTwo = currentToolPool[Math.floor(Math.random() * currentToolPool.length)];
+            setToolTwo(randomToolTwo);
+            currentToolPool = currentToolPool.filter((tool) => tool.name !== randomToolTwo.name);
+        }
+        //Tool 3
+        randomToolThree = currentToolPool[Math.floor(Math.random() * currentToolPool.length)];
+        setToolThree(randomToolThree);
+        currentToolPool = currentToolPool.filter((tool) => tool.name !== randomToolThree.name);
 
+        //Tool 4
+        randomToolFour = currentToolPool[Math.floor(Math.random() * currentToolPool.length)];
+        setToolFour(randomToolFour);
 
+        //Roll Consumables
+        let randomConsumableOne = '';
+        let randomConsumableTwo = '';
+        let randomConsumableThree = '';
+        let randomConsumableFour = '';
+
+        //Consumable One
+        randomConsumableOne = consumables[Math.floor(Math.random() * consumables.length)];
+        setConsumableOne(randomConsumableOne);
+
+        //Consumable Two
+        randomConsumableTwo = consumables[Math.floor(Math.random() * consumables.length)];
+        setConsumableTwo(randomConsumableTwo);
+
+        //Consumable Three
+        randomConsumableThree = consumables[Math.floor(Math.random() * consumables.length)];
+        setConsumableThree(randomConsumableThree);
+
+        //Consumable Four
+        randomConsumableFour = consumables[Math.floor(Math.random() * consumables.length)];
+        setConsumableFour(randomConsumableFour);
     }
 
     return (
@@ -109,6 +230,11 @@ const LoudoutRandomizer = () => {
             </h4>
             <h2 className="test">Name: {weaponOne.name} / Ammo Type: {weaponOneAmmo}</h2>
             <h2 className="test">Name: {weaponTwo.name} / Ammo Type: {weaponTwoAmmo}</h2>
+            <h2 className="test">Tool 1: {toolOne.name}, Tool 2: {toolTwo.name}, Tool 3: {toolThree.name}, Tool 4: {toolFour.name}</h2>
+            <h2 className="test">C1: {consumableOne.name}, C2: {consumableTwo.name}, C3: {consumableThree.name}, C4: {consumableFour.name}</h2>
+            <button onClick={() => setFixedMedkit(!fixedMedkit)}>{fixedMedkit ? 'Disable Fixed Medkit' : 'Enable Fixed Medkit '}</button>
+            <button onClick={() => setFixedMeleeTool(!fixedMeleeTool)}>{fixedMeleeTool ? 'Disable Fixed Melee Tool' : 'Enable Fixed Melee Tool '}</button>
+            <button onClick={() => setCustomAmmo(!customAmmo)}>{customAmmo ? 'Disable Custom Ammo' : 'Enable Custom Ammo '}</button>
             <button onClick={() => setQuartermaster(!quartermaster)}>{quartermaster ? 'Disable Quartermaster' : 'Enable Quartermaster '}</button>
             <button onClick={() => handleDisableScopes()}>{disableScopes ? 'Enable Scopes' : 'Disable Scopes'}</button>
             <button onClick={() => roll()}>GENERATE</button>
